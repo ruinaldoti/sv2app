f236e7f76bc3ac1f1a6b47dac8302089b7dfb11b
ruinadoti <ruinaldo.ti@gmail.com>
2019-11-25 16:27:59 -0300

    Add tabela apartirt das class

--- src/main/java/com/sv2app/resources/CategoriaResource.java
+++ src/main/java/com/sv2app/resources/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.sv2app.resources;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+	
+	
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public String listar() {
