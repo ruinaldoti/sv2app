4b37852fb3de1b6af9eeb12c623d7f01c8f1ca8e
ruinadoti <ruinaldo.ti@gmail.com>
2019-11-26 17:06:03 -0300

    inserindo novaCategoria com POST

--- src/main/java/com/sv2app/resources/CategoriaResource.java
+++ src/main/java/com/sv2app/resources/CategoriaResource.java
@@ -1,11 +1,17 @@
 package com.sv2app.resources;
 
+import java.net.URI;
+
+import javax.servlet.Servlet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sv2app.domain.Categoria;
 import com.sv2app.services.CategoriaService;
@@ -22,6 +28,13 @@ public class CategoriaResource {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	
--- src/main/java/com/sv2app/services/CategoriaService.java
+++ src/main/java/com/sv2app/services/CategoriaService.java
@@ -26,6 +26,11 @@ public class CategoriaService {
 				"Objeto não encobtrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
 	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 	
 	
 }
