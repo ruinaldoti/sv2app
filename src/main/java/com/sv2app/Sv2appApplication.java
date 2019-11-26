636533adfae559b12ab97474778726287ae7cbe7
ruinadoti <ruinaldo.ti@gmail.com>
2019-11-25 21:00:04 -0300

    Estados e Cidade

--- src/main/java/com/sv2app/Sv2appApplication.java
+++ src/main/java/com/sv2app/Sv2appApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sv2app.domain.Categoria;
+import com.sv2app.domain.Cidade;
+import com.sv2app.domain.Estado;
 import com.sv2app.domain.Produto;
 import com.sv2app.repositories.CategoriaRepository;
+import com.sv2app.repositories.CidadeRepository;
+import com.sv2app.repositories.EstadoRepository;
 import com.sv2app.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class Sv2appApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Sv2appApplication.class, args);
@@ -45,6 +55,20 @@ public class Sv2appApplication implements CommandLineRunner {
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		*/
+		
+		Estado est1 = new Estado(null, "Mina Gerais");
+		Estado est2 = new Estado(null, "Ceara");
+		
+		Cidade cida1 = new Cidade(null, "Fortaleza", est2);
+		Cidade cida2 = new Cidade(null, "Sobral", est2);
+		Cidade cida3 = new Cidade(null, "Ocara", est2);
+		Cidade cida4 = new Cidade(null, "Uberlandia", est1);
+		
+		est1.getCidades().addAll(Arrays.asList(cida4));	
+		est2.getCidades().addAll(Arrays.asList(cida1, cida2, cida3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(cida1, cida2, cida3, cida4));
 		
 		
 	}
