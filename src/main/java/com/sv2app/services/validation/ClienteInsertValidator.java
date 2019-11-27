38366b75ba3849d996255c52f9ff1c43f622a455
ruinadoti <ruinaldo.ti@gmail.com>
2019-11-27 19:35:58 -0300

    Validacao de Email

--- src/main/java/com/sv2app/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/sv2app/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sv2app.domain.Cliente;
 import com.sv2app.domain.enums.TipoCliente;
 import com.sv2app.dto.ClienteNewDTO;
+import com.sv2app.repositories.ClienteRepository;
 import com.sv2app.resources.exception.FieldMessage;
 import com.sv2app.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -29,16 +37,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 
 
-/*
 		Cliente aux = repo.findByEmail(objDto.getEmail());
-
 		if (aux != null) {
-
 			list.add(new FieldMessage("email", "Email já existente"));
-
 		}
 		
-	*/	
+	
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
